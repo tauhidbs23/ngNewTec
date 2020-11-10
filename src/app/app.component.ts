3529244a2eeee1ec7a226d6c0709433a51513665
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-11-10 16:44:06 +0600

    phone no validation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngNewTec';
+  userAddressValidations: FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
+  ngOnInit() {
+    this.userAddressValidations = this.fb.group({
+      mobile: ['', Validators.compose([Validators.pattern(
+      '(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})'
+        )])]
+    });
+
+  }
 }
