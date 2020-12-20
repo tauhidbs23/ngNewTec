6583ef564d3354595027f2bb2e8773a591c2c8dd
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-12-21 00:53:00 +0600

    nested try catch works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngNewTec';
+  // https://stackoverflow.com/questions/10727025/javascript-pass
+  myVariable = 'fyf';
+
+
+  ngOnInit() {
+    try {
+      if (0 > 1) {
+        console.log('three > one');
+        try {
+          if (5 > 6) {
+            console.log('five > six');
+          } else {
+            throw new Error('i am chaild error, throw to next catch')
+          }
+        } catch (error) {
+          throw error
+          // console.log(error);
+        }
+      } else {
+        throw new Error('i am parent error')
+      }
+    } catch (error) {
+      console.log('i am parrent ')
+      console.error(error)
+    }
+  }
+
 }
