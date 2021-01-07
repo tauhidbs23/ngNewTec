2f4622988beaf2b5268ce20158b3868fc9e9ee17
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-01-07 12:59:29 +0600

    nested try catch works fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,47 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit() {
+    this.outerFunction();
+    // try {
+    //   if (0 > 1) {
+    //     console.log('three > one');
+    //     try {
+    //       if (5 > 6) {
+    //         console.log('five > six');
+    //       } else {
+    //         throw new Error('i am chaild error, throw to next catch')
+    //       }
+    //     } catch (error) {
+    //       throw error
+    //       // console.log(error);
+    //     }
+    //   } else {
+    //     throw new Error('i am parent error')
+    //   }
+    // } catch (error) {
+    //   console.log('i am parrent ')
+    //   console.error(error)
+    // }
+  }
+
+  innerFunction(callback){
+    return callback(true);
+  }
+
+  outerFunction(){
     try {
-      if (0 > 1) {
-        console.log('three > one');
-        try {
-          if (5 > 6) {
-            console.log('five > six');
-          } else {
-            throw new Error('i am chaild error, throw to next catch')
-          }
-        } catch (error) {
-          throw error
-          // console.log(error);
+      this.innerFunction(callback=>{
+        if(callback){
+          throw new Error('sss');
+
+
         }
-      } else {
-        throw new Error('i am parent error')
-      }
+      });
     } catch (error) {
-      console.log('i am parrent ')
-      console.error(error)
+      console.log(error);
+      
     }
+
   }
 
 }
